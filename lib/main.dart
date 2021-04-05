76923c6556fe438c79048f333143884f18e2d651
Lalit Negi <lnegi@sapient.com>
2021-04-05 17:19:37 +0100

    Click handler without state

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,17 @@ void main() {
  * or StatefulWidget
  */
 class MyWidgetsApp extends StatelessWidget {
+  var idx = 0;
+  var questions = ["what's your hair color?",  "What's your eye color?"];
+  
+  void answerHandler() {
+    if (idx == 1) {
+      idx = 0;
+    } else {
+      idx++;
+    }
+  }
+
   /**
    * build method has to be overriden / implemented here which returns a widget of MaterialApp
    * which has a nested widget Text rendered as home
@@ -25,11 +36,29 @@ class MyWidgetsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter app1'),
-        ), // header trailing comma for formatting
-        body: Text('Body of the app'),
-      ),
+          appBar: AppBar(
+            title: Text('Flutter app1'),
+          ), // header trailing comma for formatting
+          body: Column(
+            children: [
+              Text(questions[idx]),
+              RaisedButton(
+                child: Text('Black'),
+                onPressed: answerHandler,
+              ),
+              RaisedButton(
+                child: Text('Grey'),
+                onPressed: answerHandler,
+              ),
+              RaisedButton(
+                child: Text('Brown'),
+                onPressed: answerHandler,
+              ),
+            ],
+          )
+          // body: Text('Body of the app'),
+
+          ),
     );
     // return MaterialApp(home: Text('Hello world'),);
     // // TODO: implement build
